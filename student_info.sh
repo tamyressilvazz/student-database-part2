3c52690352fa3f7a1374bf01a19e980fe6f128f1
Tamyres silvazz <tamyressilvazz@outlook.com>
2025-05-24 06:04:40 +0000

    Save progress: 2440.1

--- student_info.sh
+++ student_info.sh
@@ -35,3 +35,5 @@ echo "$($PSQL "SELECT major FROM students FULL JOIN majors ON students.major_id 
 
 echo -e "\nList of unique courses, in reverse alphabetical order, that no student or 'Obie Hilpert' is taking:"
 echo "$($PSQL "SELECT DISTINCT(course) FROM students RIGHT JOIN majors USING(major_id) INNER JOIN majors_courses USING(major_id) INNER JOIN courses USING(course_id) WHERE (first_name = 'Obie' AND last_name = 'Hilpert') OR student_id IS NULL ORDER BY course DESC")"
+
+echo -e "\nList of courses, in alphabetical order, with only one student enrolled:"
